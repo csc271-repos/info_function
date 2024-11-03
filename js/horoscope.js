995f6cc02a17ad4501d1bce60471f6ab8a59c2d9
csc372 <141356425+csc372@users.noreply.github.com>
2024-11-03 11:10:08 -0500

    Add files via upload

---
+++ js/horoscope.js
@@ -0,0 +1,18 @@
+function displayTitle() {
+	var title = '<h1>Cosmic Horoscopes</h1>';
+	var contentDiv = document.getElementById('title');
+	contentDiv.innerHTML += title;
+};
+
+
+
+
+displayTitle();
+
+
+
+
+
+
+
+
